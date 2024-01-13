a4553303f75349798f19d5d875d4e2ebc8ad3ed3
Victoria Haidei <haideivctoria@gmail.com>
2024-01-13 13:19:58 +0200

    Initial commit

---
+++ src/W1.java
@@ -0,0 +1,14 @@
+public class W1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+
+
+        System.out.println(a);
+        System.out.println(b);
+}}
+
