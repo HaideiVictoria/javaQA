1af03d5f7926a833caacbb13a1a98101b2e4860a
Victoria Haidei <haideivctoria@gmail.com>
2024-01-29 13:20:38 +0200

    Initial commit

---
+++ src/HW4/Hw4.java
@@ -0,0 +1,33 @@
+package HW4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hw4 {
+    public static void main(String[] args) {
+       //Есть одномерный массив из 10 элементов, заполнен- ный случайными числами. Пользователь вводит с клавиатуры число.
+        // Программа показывает есть ли такое число в созданном до этого массиве.
+
+        int[] array = new int[10];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+            System.out.println("Масив: " + Arrays.toString(array));
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("ВВедіть число");
+            int user= scanner.nextInt();
+            boolean found= false;
+            for (int number :array ){
+                if(number==user){
+                    found=true;
+                    break;
+                }}
+            if (found){
+                System.out.println("число"+user+"знайдено");
+            }else {
+                System.out.println("число"+user+"не знайдено");
+            }
+    }}}
+
