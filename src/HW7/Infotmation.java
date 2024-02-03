4381b3d2334ee6b12f1dc0c0b9ad5a4100fe13be
Victoria Haidei <haideivctoria@gmail.com>
2024-02-03 13:02:51 +0200

    Initial commit

---
+++ src/HW7/Infotmation.java
@@ -0,0 +1,16 @@
+package HW7;
+
+public class Infotmation {
+    public static void main(String[] args) {
+        Task3 information=new Task3();
+        information.setLastname("Parker");
+        information.setSubjectName("QA");
+        information.setNumbersOfPages(100);
+        information.setYears(6);
+        information.setColor("red");
+        information.setNameOfInstitution("DAN.IT.Educarion");
+        System.out.println(information.lastname +" " +information.subjectName+" "+information.numbersOfPages+" "+information.years+" "+information.color+" "+information.nameOfInstitution);
+
+
+    }
+}
